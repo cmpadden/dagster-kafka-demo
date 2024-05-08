5cbfaef9b8e030a297b7221b50b37dd1bec98c98
Colton Padden <colton@dagsterlabs.com>
2024-05-08 16:05:40 -0400

    use MaterializeResult

--- dagster_kafka_demo/assets.py
+++ dagster_kafka_demo/assets.py
@@ -1,7 +1,14 @@
 import time
 from typing import List
 
-from dagster import AssetSelection, Config, OpExecutionContext, asset, define_asset_job
+from dagster import (
+    AssetSelection,
+    Config,
+    MaterializeResult,
+    OpExecutionContext,
+    asset,
+    define_asset_job,
+)
 
 
 class MyAssetConfig(Config):
@@ -9,13 +16,19 @@ class MyAssetConfig(Config):
 
 
 @asset
-def loaded_from_kafka(context: OpExecutionContext, config: MyAssetConfig):
+def loaded_from_kafka(context: OpExecutionContext, config: MyAssetConfig) -> MaterializeResult:
     context.log.info(f"Handling kafka batch with values {config.batch}")
-
-    context.add_output_metadata(metadata={"kafka_message_value": str(config.batch)})
 
     # do the real processing here
     time.sleep(1)
+
+    return MaterializeResult(
+        metadata={
+            "kafka_batch_size": len(config.batch),
+            "kafka_batch_value_start": config.batch[0],
+            "kafka_batch_value_end": config.batch[-1],
+        }
+    )
 
 
 downstream_of_kafka = define_asset_job(
